fda125ba2834931ff55ab9339f15970d7a0746f4
Darkness-Monster <darknessmonster60@gmail.com>
2023-08-31 02:54:59 +0200

    Setup

---
+++ app/src/main/java/com/darknessmonster/a2dandroidstudiogame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.darknessmonster.a2dandroidstudiogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
